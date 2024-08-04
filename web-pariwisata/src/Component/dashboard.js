bf6056221acb76533c5a23dcaaac0adf832ac776
Fahri Aminuddin Abdillah <fahriaminnudin@gmail.com>
2024-08-04 16:20:24 +0700

    adding the necessary

---
+++ web-pariwisata/src/Component/dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Dash = () => {
+
+    return (
+        <div>
+            Sebuah Website
+        </div>
+    )
+
+}
+
+export default Dash;
