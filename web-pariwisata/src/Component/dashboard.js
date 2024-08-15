95bfef51f3f316ed563af739ecda85a9a1a61e22
yosepafrr <yosepadrianafauziramdani@gmail.com>
2024-08-15 14:54:03 +0700

    dashboard UI

--- web-pariwisata/src/Component/dashboard.js
+++ web-pariwisata/src/Component/dashboard.js
@@ -2,9 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import bg from "../background.png";
 import '../App.css';
-import py from "../Images/panyangrayan.png";
-import karang from "../Images/karang.png";
-import ari from "../Images/ari.png";
+import resik from "../Images/karangresik/karangresik1.jpg";
+import resik2 from "../Images/karangresik/karangresik2.jpg";
+import pointer from "../Images/map-pointer.png";
+import arrow from "../Images/ARROW BLUE.png";
+import arrowwh from "../Images/ARROW WHITE.png";
+import karang from "../Images/karangtawulan/IMG_8663.JPG";
+import ari from "../Images/dinding/IMG_1052.JPG";
 import ar2 from "../Images/ar2.png";
 import kr from "../Images/kr2.png"
 import rs from "../Images/rs.png"
@@ -14,27 +18,53 @@ const Dash = () => {
     return (
         <>
         <body>
-            <section className="estasik" id="estasik">
-                <div className="container">
-                    <img className="image" src={bg}/>   {/* Coba ganti ya, bang. Bingung mau background imagenya */}
-                </div>
+            {/* HERO SECTION START */}
+            <section className="h-[120vh] mb-20 py-52 px-32" id="estasik">
+                <container className="relative">
+                        <a href="https://maps.app.goo.gl/q3tgBZsnTqt938Tq5" target="blank" className="items-end"><img className="absolute right-0 w-16 items-end" src={pointer}></img></a>
+                        <div className="text-[#137FB8] text-7xl">
+                            <h1 className="mb-10">tasik <br/><span className="text-9xl font-bold">estetik,</span></h1>
+                            <div className="flex gap-2 items-center">
+                            <a href="#popular" className="text-4xl font-[300]">go <span className="font-bold">explore</span></a>
+                            <img className="w-8 item-end" src={arrow}></img>
+                            </div>
+                        </div>
+                </container>
             </section>
+            {/* HERO SECTION END */}
 
 
 
-            <section className="popular" id="popular">
-                <div>
-                    <div className="mx-auto text-center">
-                        <h1 className="poppins-thin mt-5 mb-3">POPULAR <span>DESTINATION</span></h1>                        
-                    </div>
+        {/* POPULAR SECTION START */}
+            <section className="popular pt-16 h-[100vh]" id="popular">
+                <div className="flex flex-col items-center justify-center text-[#137FB8]">
+                    <h1 className="text-4xl font-[300]"><span className="font-[700]">POPULAR </span>DESTINATION</h1>
 
-                    <div className="line mx-auto mb-2"></div>
+                    <div className="line my-7"></div>
 
                     <div className="container flex justify-center mx-auto">
-                        <div className="flex flex-row justify-items-center">
-                            <div className="basis-1/3 me-6 mt-4">
+                        <div className="flex justify-items-center gap-10">
+                            <div className="relative"> {/* IMAGE BOX */}
+                                <img className="w-[500px]" src={ari}/>
+                                <div className="absolute inset-0 mt-96 flex items-center justify-center bg-[#137FB8] bg-opacity-65"><h1 className="text-center text-xl font-[700] text-white">GUNUNG <br/><span className="font-[300]">DINDING ARI</span></h1></div>
+                            </div>
+                            <div className="relative"> {/* IMAGE BOX */}
+                                <img className="w-[500px]" src={karang}/>
+                                <div className="absolute inset-0 mt-96 flex items-center justify-center bg-[#137FB8] bg-opacity-65"><h1 className="text-center text-xl font-[700] text-white">PANTAI <br/><span className="font-[300]">KARANG TAWULAN</span></h1></div>
+                            </div>
+                            <div className="relative"> {/* IMAGE BOX */}
+                                <img className="w-[500px]" src={resik}/>
+                                <div className="absolute inset-0 mt-96 flex items-center justify-center bg-[#137FB8] bg-opacity-65"><h1 className="text-center text-xl font-[700] text-white">BUATAN <br/><span className="font-[300]">KARANG RESIK</span></h1></div>
+                            </div>
+                            </div>
+                            </div>
+                            <a href="" className="flex justify-center items-center gap-2 py-14 text-[#137FB8]">
+                                <h1 className="text-4xl font-[300]">More <span className="font-bold">Destinations</span></h1>
+                                <img className="w-8 item-end" src={arrow}></img>
+                            </a>
+                            {/* <div className="basis-1/3 me-6 mt-4">
                                 <img src={ari}/>
-                                <div className=" bg-blue-400 relative bottom-24 me-3 text-center pt-6 pb-2 opacity-35">
+                                <div className=" bg-blue-400 relative bottom-24 me-3 text-center pt-6 pb-4 opacity-35">
                                     <br></br>
                                     <br></br>
                                 </div>
@@ -55,22 +85,56 @@ const Dash = () => {
                                     <br></br>
                                 </div>
                                 <h1 className="relative bottom-40 text-center text-white">BUATAN <br></br> <span>PANYANGRAYAN</span> </h1>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="mx-auto text-center mb-6">
-                        <h1>More <span>Destinations</span> </h1>
-                    </div>
-
+                            </div> */}
                 </div>
             </section>
+            {/* POPULAR SECTION END */}
 
 
 
-            <section className="more" id="more">
+            <section className="h-[180vh] flex flex-col" id="more">
+                <div className="flex justify-between h-[60vh]">
+                    <div className="flex flex-col items-center justify-center w-[50vw] font-[300] text-white gap-5">
+                        <h1 className="title text-7xl">PEGUNUNGAN</h1>
+                        <p className="max-w-96 text-center text-xl">
+                            Salah satu pegunungan yang terkenal di <span className="font-bold">Tasikmalaya</span> adalah <span className="font-bold">galunggung</span>, 
+                            banyak spot wisata yang bisa dikunjungi seperti pemandian air panas 
+                            sampai wisata hiking <span className="font-bold">dinding ari.</span>
+                        </p>
+                        <a className="flex items-center gap-2" href="">cari lebih banyak mengenai <span className="font-bold">PEGUNUNGAN</span> <img className="w-6" src={arrowwh}/></a>
+                    </div>
+                    <img className="w-[50vw]" src={ar2}/>
+                </div>
+                <div className="flex justify-between relative h-[60vh]">
+                    <img className="items-center object-cover object-bottom blur " src={karang}/>
+                    <div className="absolute overflow-x-auto flex flex-col items-center justify-center w-full h-[60vh] font-[300] text-white gap-5">
+                        <div className="ml-28 flex justify-evenly items-center">
+                        <h1 className="title text-9xl mr-5">PANTAI</h1>
+                        <p className="max-w-[500px] text-xl">
+                        Pantai Karang Tawulan saat ini menjadi salah satu 
+                        pantai yang banyak di kunjungi di priangan timur 
+                        karena disini kamu bisa melihat sunset.
+                        </p>
+                        </div>
+                        <a className="flex items-center gap-2" href="">cari lebih banyak mengenai <span className="font-bold">PANTAI</span> <img className="w-6" src={arrowwh}/></a>
+                    </div>
+                </div>
+                <div className="flex flex-row-reverse justify-between h-[60vh]">
+                    <div className="flex flex-col items-center justify-center w-[50vw] font-[300] text-white gap-5">
+                        <h1 className="title text-7xl">WISATA BUATAN</h1>
+                        <p className="max-w-96 text-center text-xl">
+                            <span className="font-bold">Karang resik</span> menjadi <span className="font-bold">wisata buatan </span> 
+                            yang populer di <span className="font-bold">Tasikmalaya</span> karena tempat ini 
+                            memiliki unsur budaya luar seperti <span className="font-bold">jepang dan korea.</span>
+                        </p>
+                        <a className="flex items-center gap-2" href="">cari lebih banyak mengenai <span className="font-bold">WISATA BUATAN</span> <img className="w-6" src={arrowwh}/></a>
+                    </div>
+                    <img className="w-[50vw]" src={resik2}/>
+                </div>
 
-                <div className="flex flex-row justify-center text-center mx-36">
+
+
+                {/* <div className="flex flex-row justify-center text-center mx-36">
                     <div className="basis-1/2">
                         lorem ipsum dolor sit amet
                     </div>
@@ -91,7 +155,7 @@ const Dash = () => {
                         lorem ipsum dolor sit amet
                     </div>
                 </div>
-
+ */}
             </section>
         </body>
             
--- web-pariwisata/src/Component/navbar.js
+++ web-pariwisata/src/Component/navbar.js
@@ -1,33 +1,44 @@
 import React from "react";
-import Ngasik from "../Ngasik.jpg";
+import logo from "../Images/logo.png";
 
 
 const Navbar = () => {
 
     return (
+    // <>
+    //     <nav className="">
+    //     <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+    //       <div className="relative flex h-16 items-center justify-between">
+    //         <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-end">
+    //           <div className="flex flex-shrink-0 items-start me-96">
+    //             <img className="h-20 w-auto" src={logo} alt="Your Company" />
+    //           </div>
+    //           <div className="me-96">
+    //           </div>
+    //           <div className="hidden sm:ml-6 sm:block">
+    //             <div className="flex space-x-4">
+    //               <a href="#" className="rounded-md bg-gray-200 px-3 py-2 text-sm font-medium text-blue-300" aria-current="page">Home</a>
+    //               <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-blue-300 hover:bg-gray-700 hover:text-white">Pegunungan</a>
+    //               <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-blue-300 hover:bg-gray-700 hover:text-white">Pantai</a>
+    //               <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-blue-300 hover:bg-gray-700 hover:text-white">Buatan</a>
+    //             </div>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </nav>
+    // </>       
     <>
-        <nav className="">
-        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-          <div className="relative flex h-16 items-center justify-between">
-            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-end">
-              <div className="flex flex-shrink-0 items-start me-96">
-                <img className="h-8 w-auto" src={Ngasik} alt="Your Company" />
-              </div>
-              <div className="me-96">
-              </div>
-              <div className="hidden sm:ml-6 sm:block">
-                <div className="flex space-x-4">
-                  <a href="#" className="rounded-md bg-gray-200 px-3 py-2 text-sm font-medium text-blue-300" aria-current="page">Home</a>
-                  <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-blue-300 hover:bg-gray-700 hover:text-white">Pegunungan</a>
-                  <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-blue-300 hover:bg-gray-700 hover:text-white">Pantai</a>
-                  <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-blue-300 hover:bg-gray-700 hover:text-white">Buatan</a>
-                </div>
-              </div>
-            </div>
-          </div>
+      <nav className="py-5 px-40 flex justify-between items-center text-[#818CB3]">
+        <img src={logo} alt="logo" className="w-60"></img>
+        <div className="flex gap-14 text-xl">
+          <a href="#">Home</a>
+          <a href="#">Pantai</a>
+          <a href="#">Dataran Tinggi</a>
+          <a href="#">Wisata Buatan</a>
         </div>
       </nav>
-    </>       
+    </>
 
     )
 
