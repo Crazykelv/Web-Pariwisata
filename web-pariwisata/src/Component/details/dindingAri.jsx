10815eddc0e3146cd7800cb893e372c9120fcda3
Fahri Aminuddin Abdillah <fahriaminnudin@gmail.com>
2024-08-22 10:34:26 +0700

    carousel

--- web-pariwisata/src/Component/details/dindingAri.jsx
+++ web-pariwisata/src/Component/details/dindingAri.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ari1 from "../../Images/dinding/bgDarker.JPG";
+import ari2 from "../../Images/dinding/IMG_0948.JPG";
+import ari3 from "../../Images/dinding/IMG_0949.JPG";
+import ari4 from "../../Images/dinding/IMG_1052.JPG";
 import arrowwh from "../../Images/ARROW WHITE.png";
-
-
+import Carousel from './carousel.component';
 
 const dindingAri = () => {
+
+  let slides = [
+    ari2,
+    ari1,
+    ari3,
+    ari4,
+  ];
+
   return (
     <>
         <section style={{
@@ -22,6 +32,12 @@ const dindingAri = () => {
               <h1 className='font-extrabold text-9xl'>DINDING ARI</h1>
               <a className='font-normal text-2xl outline px-5 py-1' href="">LIHAT GALERI</a>
             </div>
+        </section>
+
+        <section>
+          <div className='w-full m-auto'>
+            <Carousel slides={slides} />
+          </div>
         </section>
     </>
   )
