4c70a9d67d7597c2a7c46f171206cab15d730e22
yosepafrr <yosepadrianafauziramdani@gmail.com>
2024-08-30 12:52:38 +0700

    Responsive done

--- web-pariwisata/src/Component/dashboard.jsx
+++ web-pariwisata/src/Component/dashboard.jsx
@@ -17,13 +17,13 @@ const Dash = () => {
         <>
         <body>
             {/* HERO SECTION START */}
-            <section className="h-[120vh] mb-20 py-52 px-32" id="estasik">
+            <section className="h-[120vh] tablet:h-[80vh] android:h-[40vh] iphone:h-[30vh] mb-20 py-52 android:py-28 iphone:py-14 px-32 tablet-sm:px-14 tablet:px-20 iphone:px-10" id="estasik">
                 <container className="relative">
-                        <a href="https://maps.app.goo.gl/q3tgBZsnTqt938Tq5" target="blank" className="items-end"><img className="absolute right-0 w-16 items-end" src={pointer}></img></a>
-                        <div className="text-[#137FB8] text-7xl">
-                            <h1 className="mb-10">tasik <br/><span className="text-9xl font-bold">estetik,</span></h1>
+                        <a href="https://maps.app.goo.gl/q3tgBZsnTqt938Tq5" target="blank" className="items-end"><img className="absolute right-0 tablet-sm:hidden w-16 items-end" src={pointer}></img></a>
+                        <div className="text-[#137FB8] text-7xl tablet:text-5xl android:text-3xl iphone:text-xl px-3">
+                            <h1 className="mb-10 tablet:mb-5 iphone:mb-2">tasik <br/><span className="text-9xl tablet:text-8xl android:text-6xl iphone:text-5xl font-bold">estetik,</span></h1>
                             <div className="flex gap-2 items-center">
-                            <a href="#popular" className="text-4xl font-[300]">go <span className="font-bold">explore</span></a>
+                            <a href="#popular" className="text-4xl tablet:text-3xl android:text-xl font-[300]">go <span className="font-bold">explore</span></a>
                             <img className="w-8 item-end" src={arrow}></img>
                             </div>
                         </div>
@@ -34,25 +34,25 @@ const Dash = () => {
 
 
         {/* POPULAR SECTION START */}
-            <section className="popular pt-16 max-h-[95vh]" id="popular">
+            <section className="popular pt-16 max-h-[95vh] tablet:max-h-[350vh]" id="popular">
                 <div className="flex flex-col items-center justify-center text-[#137FB8]">
-                    <h1 className="text-4xl font-[300]"><span className="font-[700]">POPULAR </span>DESTINATION</h1>
+                    <h1 className="text-4xl android:text-3xl android-sm:text-2xl font-thin"><span className="font-bold">POPULAR </span>DESTINATION</h1>
 
                     <div className="line my-7"></div>
 
-                    <div className="container flex justify-center mx-auto">
-                        <div className="flex justify-items-center gap-10">
+                    <div className="flex justify-center mx-[3%]">
+                        <div className="flex justify-items-center tablet:flex-col gap-10">
                             <a href="/dindingAri" className="relative"> {/* IMAGE BOX */}
-                                <img className="max-w-[500px]" src={ari}/>
-                                <div className="absolute inset-0 mt-96 flex items-center justify-center bg-[#137FB8] bg-opacity-65"><h1 className="text-center text-xl font-[700] text-white">GUNUNG <br/><span className="font-[300]">DINDING ARI</span></h1></div>
+                                <img className="w-[500px] tablet:w-[90vh] tablet:h-[40vh] android:h-[25vh] object-cover " src={ari}/>
+                                <div className="absolute inset-0 mt-[70%] tablet-sm:mt-[40%] tablet:mt-[30%] android:mt-[30%] iphone:mt-[50%] flex items-center justify-center bg-[#137FB8] bg-opacity-65"><h1 className="text-center text-xl font-[700] text-white">GUNUNG <br/><span className="font-[300]">DINDING ARI</span></h1></div>
                             </a>
                             <div className="relative"> {/* IMAGE BOX */}
-                                <img className="max-w-[500px]" src={karang}/>
-                                <div className="absolute inset-0 mt-96 flex items-center justify-center bg-[#137FB8] bg-opacity-65"><h1 className="text-center text-xl font-[700] text-white">PANTAI <br/><span className="font-[300]">KARANG TAWULAN</span></h1></div>
+                                <img className="w-[500px] tablet:w-[90vh] tablet:h-[40vh] android:h-[25vh] object-cover object-bottom" src={karang}/>
+                                <div className="absolute inset-0 mt-[70%] tablet-sm:mt-[40%] tablet:mt-[30%] android:mt-[30%] iphone:mt-[50%] flex items-center justify-center bg-[#137FB8] bg-opacity-65"><h1 className="text-center text-xl font-[700] text-white">PANTAI <br/><span className="font-[300]">KARANG TAWULAN</span></h1></div>
                             </div>
                             <div className="relative"> {/* IMAGE BOX */}
-                                <img className="max-w-[500px]" src={resik}/>
-                                <div className="absolute inset-0 mt-96 flex items-center justify-center bg-[#137FB8] bg-opacity-65"><h1 className="text-center text-xl font-[700] text-white">BUATAN <br/><span className="font-[300]">KARANG RESIK</span></h1></div>
+                                <img className="w-[500px] tablet:w-[90vh] tablet:h-[40vh] android:h-[25vh] object-cover " src={resik}/>
+                                <div className="absolute inset-0 mt-[70%] tablet-sm:mt-[40%] tablet:mt-[30%] android:mt-[30%] iphone:mt-[50%] flex items-center justify-center bg-[#137FB8] bg-opacity-65"><h1 className="text-center text-xl font-[700] text-white">BUATAN <br/><span className="font-[300]">KARANG RESIK</span></h1></div>
                             </div>
                             </div>
                             </div>
@@ -62,47 +62,47 @@ const Dash = () => {
 
 
             <a href="#more" className="flex justify-center items-center gap-2 my-14 text-[#137FB8]">
-                                <h1 className="text-4xl font-[300]">More <span className="font-bold">Destinations</span></h1>
+                                <h1 className="text-4xl android:text-3xl android-sm:text-2xl font-[300]">More <span className="font-bold">Destinations</span></h1>
                                 <img className="w-8 item-end" src={arrow}></img>
             </a>
-            <section className="h-[180vh] flex flex-col mt-14" id="more">
-                <div className="flex justify-between h-[60vh] relative">
-                    <div className="flex flex-col items-center justify-center w-[50vw] font-[300] text-white gap-5">
-                        <h1 className="title text-7xl">PEGUNUNGAN</h1>
-                        <p className="max-w-96 text-center text-xl">
+            <section className="h-[180vh] tablet-sm:h-[120vh] android-sm:h-[90vh] flex flex-col mt-14 overflow-hidden" id="more">
+                <div className="flex justify-between h-[60vh] tablet-sm:h-[40vh] android-sm:h-[30vh] relative">
+                    <div className="flex tablet:mx-5 flex-col items-center justify-center w-[50vw] tablet:w-full font-thin text-white gap-5">
+                        <h1 className="title text-7xl tablet-sm:text-6xl android:text-5xl android-sm:text-4xl">PEGUNUNGAN</h1>
+                        <p className="max-w-96 text-center text-xl android:text-base android-sm:text-xs android-sm:max-w-72">
                             Salah satu pegunungan yang terkenal di <span className="font-bold">Tasikmalaya</span> adalah <span className="font-bold">galunggung</span>, 
                             banyak spot wisata yang bisa dikunjungi seperti pemandian air panas 
                             sampai wisata hiking <span className="font-bold">dinding ari.</span>
                         </p>
-                        <a className="flex items-center gap-2" href="/dataranTinggi">cari lebih banyak mengenai <span className="font-bold">PEGUNUNGAN</span> atau <span className="font-bold">DATARAN TINGGI</span><img className="w-6" src={arrowwh}/></a>
+                        <a className="flex items-center gap-2 android:text-xs android-sm:text-[9px] iphone:text-[7px]" href="/dataranTinggi">cari lebih banyak mengenai <span className="font-bold">PEGUNUNGAN</span> atau <span className="font-bold">DATARAN TINGGI</span><img className="w-6" src={arrowwh}/></a>
                     </div>
-                    <img className="w-[50vw]" src={ar2}/>
+                    <img className="w-[50vw] tablet:hidden" src={ar2}/>
                 </div>
-                <div className="flex justify-between relative h-[60vh]">
+                <div className="flex justify-between relative h-[60vh] tablet-sm:h-[40vh] android-sm:h-[30vh] ml-[-40px]">
                     <img className="items-center object-cover object-bottom blur" src={karang}/>
-                    <div className="absolute overflow-x-auto flex flex-col items-center justify-center w-full h-[60vh] font-[300] text-white gap-5">
-                        <div className="ml-28 flex justify-evenly items-center">
-                        <h1 className="title text-9xl mr-5">PANTAI</h1>
-                        <p className="max-w-[500px] text-xl">
+                    <div className="absolute tablet:mx-5 overflow-x-auto flex flex-col items-center justify-center w-full h-[60vh] tablet-sm:h-[40vh] android-sm:h-[30vh] font-thin text-white gap-5">
+                        <div className="ml-28 tablet:ml-0 flex justify-evenly items-center tablet:flex-col">
+                        <h1 className="title text-9xl mr-5 tablet:text-7xl tablet-sm:text-6xl android:text-5xl android-sm:text-4xl" >PANTAI</h1>
+                        <p className="max-w-[500px] android:max-w-[400px] android-sm:max-w-[300px] text-xl tablet:text-center android:text-base android-sm:text-xs">
                         Pantai Karang Tawulan saat ini menjadi salah satu 
                         pantai yang banyak di kunjungi di priangan timur 
                         karena disini kamu bisa melihat sunset.
                         </p>
                         </div>
-                        <a className="flex items-center gap-2" href="/pantai">cari lebih banyak mengenai <span className="font-bold">PANTAI</span>atau <span className="font-bold">PERAIRAN</span> <img className="w-6" src={arrowwh}/></a>
+                        <a className="flex items-center android:text-xs android-sm:text-[9px] iphone:text-[7px] gap-2" href="/pantai">cari lebih banyak mengenai <span className="font-bold">PANTAI</span>atau <span className="font-bold">PERAIRAN</span> <img className="w-6" src={arrowwh}/></a>
                     </div>
                 </div>
-                <div className="flex flex-row-reverse justify-between h-[60vh]">
-                    <div className="flex flex-col items-center justify-center w-[50vw] font-[300] text-white gap-5">
-                        <h1 className="title text-7xl">WISATA BUATAN</h1>
-                        <p className="max-w-96 text-center text-xl">
+                <div className="flex flex-row-reverse justify-between h-[60vh] tablet-sm:h-[40vh] android-sm:h-[30vh]">
+                    <div className="flex tablet:mx-5 flex-col items-center justify-center w-[50vw] tablet:w-full font-[300] text-white gap-5">
+                        <h1 className="title text-7xl tablet-sm:text-6xl android:text-5xl android-sm:text-4xl">WISATA BUATAN</h1>
+                        <p className="max-w-96 text-center text-xl android:text-base android-sm:text-xs android-sm:max-w-72">
                             <span className="font-bold">Karang resik</span> menjadi <span className="font-bold">wisata buatan </span> 
                             yang populer di <span className="font-bold">Tasikmalaya</span> karena tempat ini 
                             memiliki unsur budaya luar seperti <span className="font-bold">jepang dan korea.</span>
                         </p>
-                        <a className="flex items-center gap-2" href="/buatan">cari lebih banyak mengenai <span className="font-bold">WISATA BUATAN</span> <img className="w-6" src={arrowwh}/></a>
+                        <a className="flex items-center android:text-xs android-sm:text-[9px] iphone:text-[7px] gap-2" href="/buatan">cari lebih banyak mengenai <span className="font-bold">WISATA BUATAN</span> <img className="w-6" src={arrowwh}/></a>
                     </div>
-                    <img className="w-[50vw]" src={resik2}/>
+                    <img className="w-[50vw] tablet:hidden" src={resik2}/>
                 </div>
 
 
